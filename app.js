a15cc10651972d836650acd588b4332702b936aa
TerranceRivers <109315955+TerranceRivers@users.noreply.github.com>
2023-05-22 11:17:42 -0400

    update2

--- app.js
+++ app.js
@@ -6,11 +6,7 @@ const port = 3001;
 
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
-
-// Read all users
+// Get users
 app.get("/all-users", (req, res) => {
   res.status(200).json({ data: usersList });
 });
@@ -27,99 +23,96 @@ app.get("/single-user/:phoneNumber", (req, res) => {
   }
 });
 
-app.get('/some-users/:countryName', (req, res) => {
-    const countryName = req.params.countryName;
+app.get("/some-users/:countryName", (req, res) => {
+  const countryName = req.params.countryName;
 
-    const users = usersList.filter(user => user.location.country.toLowerCase() === countryName.toLowerCase());
+  const users = usersList.filter(
+    (user) => user.location.country.toLowerCase() === countryName.toLowerCase()
+  );
 
-    if (users.length > 0) {
-        res.json(users);
-    } else {
-        res.status(404).send('No users found from this country');
-    }
+  if (users.length > 0) {
+    res.json(users);
+  } else {
+    res.status(404).send("No users found from this country");
+  }
 });
-
-
 
 // Create a new user
 app.post("/new-user", (req, res) => {
-    const newUser = {
-      gender: req.body.gender,
-      name: {
-        title: req.body.name.title,
-        first: req.body.name.first,
-        last: req.body.name.last,
-      },
-      location: {
-        city: req.body.location.city,
-        state: req.body.location.state,
-        country: req.body.location.country,
-        postcode: req.body.location.postcode,
-      },
-      email: req.body.email,
-      phone: req.body.phone,
-      cell: req.body.cell,
-      nat: req.body.nat,
-    };
-  
-    usersList.push(newUser);
-  
-    res.status(201).json(newUser);
-  });
-  
+  const newUser = {
+    gender: req.body.gender,
+    name: {
+      title: req.body.name.title,
+      first: req.body.name.first,
+      last: req.body.name.last,
+    },
+    location: {
+      city: req.body.location.city,
+      state: req.body.location.state,
+      country: req.body.location.country,
+      postcode: req.body.location.postcode,
+    },
+    email: req.body.email,
+    phone: req.body.phone,
+    cell: req.body.cell,
+    nat: req.body.nat,
+  };
+
+  usersList.push(newUser);
+
+  res.status(201).json(newUser);
+});
 
 // Update a user
 
-app.put('/update-user/:email', (req, res) => {
-    const email = req.params.email;
+app.put("/update-user/:email", (req, res) => {
+  const email = req.params.email;
 
-    const index = usersList.findIndex(user => user.email === email);
+  const index = usersList.findIndex((user) => user.email === email);
 
-    if (index !== -1) {
-        const originalUser = usersList[index];
+  if (index !== -1) {
+    const originalUser = usersList[index];
 
-        const updatedUser = {
-            gender: req.body.gender || originalUser.gender,
-            name: {
-                title: req.body.name?.title || originalUser.name.title,
-                first: req.body.name?.first || originalUser.name.first,
-                last: req.body.name?.last || originalUser.name.last,
-            },
-            location: {
-                city: req.body.location?.city || originalUser.location.city,
-                state: req.body.location?.state || originalUser.location.state,
-                country: req.body.location?.country || originalUser.location.country,
-                postcode: req.body.location?.postcode || originalUser.location.postcode,
-            },
-            email: req.body.email || originalUser.email,
-            phone: req.body.phone || originalUser.phone,
-            cell: req.body.cell || originalUser.cell,
-            nat: req.body.nat || originalUser.nat,
-        };
+    const updatedUser = {
+      gender: req.body.gender || originalUser.gender,
+      name: {
+        title: req.body.name.title || originalUser.name.title,
+        first: req.body.name.first || originalUser.name.first,
+        last: req.body.name.last || originalUser.name.last,
+      },
+      location: {
+        city: req.body.location.city || originalUser.location.city,
+        state: req.body.location.state || originalUser.location.state,
+        country: req.body.location.country || originalUser.location.country,
+        postcode: req.body.location.postcode || originalUser.location.postcode,
+      },
+      email: req.body.email || originalUser.email,
+      phone: req.body.phone || originalUser.phone,
+      cell: req.body.cell || originalUser.cell,
+      nat: req.body.nat || originalUser.nat,
+    };
 
-        usersList[index] = updatedUser;
+    usersList[index] = updatedUser;
 
-        res.json({ success: true });
-    } else {
-        res.status(404).send('User not found');
-    }
+    res.json({ success: true });
+  } else {
+    res.status(404).send("User not found");
+  }
 });
-
 
 // Delete a user
-app.delete('/delete-user/:cellNumber', (req, res) => {
-    const cellNumber = req.params.cellNumber;
+app.delete("/delete-user/:cellNumber", (req, res) => {
+  const cellNumber = req.params.cellNumber;
 
-    const index = usersList.findIndex(user => user.cell === cellNumber);
+  const index = usersList.findIndex((user) => user.cell === cellNumber);
 
-    if (index !== -1) {
-        usersList.splice(index, 1);
-        res.json({ success: true });
-    } else {
-        res.status(404).send('User not found');
-    }
+  if (index !== -1) {
+    usersList.splice(index, 1);
+    res.json({ success: true });
+  } else {
+    res.status(404).send("User not found");
+  }
 });
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
